887e513b34ad21abb19c6a37cbb51ee7830b9d33
joao.victor.p.silva <joao.victor.p.silva>
2021-03-26 17:16:22 -0300

    feat: filter by type

--- src/app/component/filter-menu/filter-menu.component.ts
+++ src/app/component/filter-menu/filter-menu.component.ts
@@ -1,3 +1,6 @@
+import { Pokemon } from './../../models/pokemon.model';
+import { Pokedex } from './../../stores/pokedex.store';
+import { TYPEENUM } from './../../enum/types-svg.enum';
 import { Component } from '@angular/core';
 import { PageStore } from 'src/app/stores/pages.store';
 
@@ -7,10 +10,33 @@ import { PageStore } from 'src/app/stores/pages.store';
   styleUrls: ['./filter-menu.component.scss'],
 })
 export class FilterMenuComponent {
-
+  public TYPEENUM: TYPEENUM = new TYPEENUM();
+  public mainFilter:string = "";
   constructor(
-    public pageStore: PageStore
+    public pageStore: PageStore,
+    private pokedex: Pokedex
   ) { }
 
+    public filterPokemon(): void {
+      this.pokedex.filteredPokemon = this.pokedex.allPokemon.filter((pokemon:Pokemon) => pokemon.type.includes(this.typeName(this.mainFilter)));
+      this.closeFilter();
+    }
 
+    public typeName(type:string):string {
+      return this.TYPEENUM.TYPENAME.find((typeName:string) => {
+        if(type.includes(typeName)){
+          return typeName;
+        }
+      });
+    }
+
+    public clearFilter():void {
+      this.mainFilter = "";
+      this.pokedex.filteredPokemon = [];
+      this.closeFilter();
+    }
+
+    public closeFilter(): void {
+      this.pageStore.isFilterMenuOpen = false;
+    }
 }
---
+++ src/app/enum/types-svg.enum.ts
@@ -0,0 +1,44 @@
+export class TYPEENUM {
+    public TYPESPATH: string [] = [
+        "../../assets/svg/types/bug.svg",
+        "../../assets/svg/types/dark.svg",
+        "../../assets/svg/types/dragon.svg",
+        "../../assets/svg/types/electric.svg",
+        "../../assets/svg/types/fairy.svg",
+        "../../assets/svg/types/fighting.svg",
+        "../../assets/svg/types/fire.svg",
+        "../../assets/svg/types/flying.svg",
+        "../../assets/svg/types/ghost.svg",
+        "../../assets/svg/types/grass.svg",
+        "../../assets/svg/types/ground.svg",
+        "../../assets/svg/types/ice.svg",
+        "../../assets/svg/types/normal.svg",
+        "../../assets/svg/types/poison.svg",
+        "../../assets/svg/types/psychic.svg",
+        "../../assets/svg/types/rock.svg",
+        "../../assets/svg/types/steel.svg",
+        "../../assets/svg/types/water.svg"
+    ];
+
+    public TYPENAME: string [] = [
+        "bug",
+        "dark",
+        "dragon",
+        "electric",
+        "fairy",
+        "fighting",
+        "fire",
+        "flying",
+        "ghost",
+        "grass",
+        "ground",
+        "ice",
+        "normal",
+        "poison",
+        "psychic",
+        "rock",
+        "steel",
+        "water"
+    ];
+
+}
